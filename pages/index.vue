31d34fc250c30586775ab53b0b8e845dd549b1e9
yukunxjr <yukunxjr@yahoo.co.jp>
2022-09-12 10:29:13 +0900

    firstcommit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <AddTodo @submit="addTodo"/>
+    <TodoList :todos="todos"/>
+  </div>
+</template>
+
+<script>
+import AddTodo from '../components/AddTodo.vue';
+import TodoList from '../components/TodoList.vue';
+export default {
+    components: { AddTodo, TodoList },
+    data(){
+      return{
+        todos:[],
+      };
+    },
+    methods:{
+      addTodo(title){
+        this.todos.push({
+          title
+        });
+      },
+    },
+}
+</script>
